da730cc62aee8c8167be51c35aa0265ec44cfaa9
Masahiro Hayashi <mhayashi1120@gmail.com>
2022-12-07 14:27:31 +0900

    upgrade package settings

--- package.scm
+++ package.scm
@@ -4,7 +4,7 @@
 
 (define-gauche-package "Gauche-net-oauth"
   ;; 
-  :version "0.6.0"
+  :version "0.6.1"
 
   ;; Description of the package.  The first line is used as a short
   ;; summary.
@@ -15,13 +15,15 @@
   ;; Example:
   ;;     :require (("Gauche" (>= "0.9.5"))  ; requires Gauche 0.9.5 or later
   ;;               ("Gauche-gl" "0.6"))     ; and Gauche-gl 0.6
-  :require ()
+  :require (("Gauche" (>= "0.9.12")))
 
   ;; List of providing modules
   ;; NB: This will be recognized >= Gauche 0.9.7.
   ;; Example:
   ;;      :providing-modules (util.algorithm1 util.algorithm1.option)
-  :providing-modules ()
+  :providing-modules (
+                      net.oauth
+                      )
   
   ;; List name and contact info of authors.
   ;; e.g. ("Eva Lu Ator <eval@example.com>"
