b41bd6a9b39ab56a1db970c47a91cdc8a89916da
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-04-06 17:58:50 +0900

    cleanup test code

--- test.scm
+++ test.scm
@@ -5,18 +5,9 @@
 (use gauche.test)
 
 (test-start "net.oauth")
+
 (use net.oauth)
 (test-module 'net.oauth)
 
-;; The following is a dummy test code.
-;; Replace it for your tests.
-;; (test* "test-net_oauth" "net_oauth is working"
-;;        (test-net_oauth))
 
-;; If you don't want `gosh' to exit with nonzero status even if
-;; the test fails, pass #f to :exit-on-failure.
 (test-end :exit-on-failure #t)
-
-
-
-
